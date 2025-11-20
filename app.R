ae4d9c47b3d34ba505091dad4bbc77e85116db72
E Ek <eek.datasc@gmail.com>
2025-11-20 03:39:21 +0000

    add first configuration files

---
+++ app.R
@@ -0,0 +1,4 @@
+message("Hello from R in VS Code on Linux!")
+x <- 1:5
+print(mean(x))
+
---
+++ config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install r-base
+
+FILE="settings.json"
+
+cat <<EOL > $FILE
+{
+  "r.rterm.linux": "/usr/bin/R",
+  "r.rpath.linux": "/usr/bin/R",
+  "r.bracketedPaste": true,
+  "r.sessionWatcher": true,
+  "r.plot.useHttpgd": true
+}
+EOL
+
+echo "settings.json has been created successfully."
+
+
+FILETESTR="app.R"
+
+cat <<'EOD' > "$FILETESTR"
+message("Hello from R in VS Code on Linux!")
+x <- 1:5
+print(mean(x))
+EOD
+
+
+echo "Installing languageserver package in R..."
+sudo Rscript -e 'install.packages("languageserver", repos="https://cloud.r-project.org")'
+sudo Rscript -e 'install.packages("httpgd", repos="https://cloud.r-project.org")'
